a8027f1093745348d2ab59311b9666de6d76ac02
yaoki87 <878787yao@gmail.com>
2021-10-15 10:09:27 +0000

    3-9投稿機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,28 @@
 class BooksController < ApplicationController
   def index
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def new
+    @book = Book.new
   end
 
   def edit
   end
+
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to book_path(id: @book)
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title,:body)
+  end
+
 end
