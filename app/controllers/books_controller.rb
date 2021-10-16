1a8eac37aa866ef7b181c1e199c66a303a56a311
yaoki87 <878787yao@gmail.com>
2021-10-16 10:06:43 +0000

    3-9Finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,18 +22,19 @@ class BooksController < ApplicationController
      flash[:notice] = "Book was successfully created."
      redirect_to book_path(id: @book)
    else
-     redirect_to books_path
+     @books = Book.all
+     render :index
    end
   end
 
   def update
 
-    book = Book.find(params[:id])
-    if book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
       flash[:notice] = "Book was successfully updated."
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
     else
-      redirect_to book_path(book.id)
+      render :edit
     end
   end
 
