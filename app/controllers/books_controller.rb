1d9ffa9872994765c260a3629ddfb93f9be09ea0
yaoki87 <878787yao@gmail.com>
2021-10-16 07:13:01 +0000

    3-9バリデーション前

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,17 +13,38 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def create
     @book = Book.new(book_params)
-    @book.save
-    redirect_to book_path(id: @book)
+   if @book.save
+     flash[:notice] = "Book was successfully created."
+     redirect_to book_path(id: @book)
+   else
+     redirect_to books_path
+   end
+  end
+
+  def update
+
+    book = Book.find(params[:id])
+    if book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(book.id)
+    else
+      redirect_to book_path(book.id)
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   private
   def book_params
     params.require(:book).permit(:title,:body)
   end
-
 end
